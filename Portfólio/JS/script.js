bff83dcf0e594ddfe175c468b374c517198a6e1c
Elialfredo <144073652+Elialfredo@users.noreply.github.com>
2023-09-10 01:18:11 +0100

    Add files via upload

---
+++ Portfólio/JS/script.js
@@ -0,0 +1,9 @@
+function mostrarmenu() {
+    let menumobile = document.querySelector('.menu-mobile');
+    menumobile.style.display = "block";
+}
+
+function fecharmenu() {
+    let menumobile = document.querySelector('.menu-mobile');
+    menumobile.style.display = "none";
+}
